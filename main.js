88f09da5f2fb71052cf6115d81a12b3a88c21189
sukanya10991 <78801848+sukanya10991@users.noreply.github.com>
2021-04-22 17:07:52 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,14 +7,28 @@ pizzas["Blackpink pizza",
        "Reading Pizza"];
   
 function getmenu(){
+    var hh;
+    var pizzas_length=pizzas.length;
+    var hh="<ol class='i_an'>";
     pizzas.sort();
-    document.getElementById("display_menu").innerHTML = pizzas;
-    console.log(pizzas);
+    for (var o=0;o<pizzas_length;o++){
+        hh=hh+"</li>"+pizzas[o]+"</li>"
+    }
+    hh=hh+"</ol>"
+    document.getElementById("display_menu").innerHTML= hh;
+    
+
 }
 function additem(){
-    add=additem.value;
+    var strawberry;
+    add=document.getElementById("additem").value;
     pizzas.push("add");
+    strawberry="<section class='cards'>";
+    for(var d=0;d<pizzas_length;d++){
+        strawberry=strawberry+"<div class='card'>"+"<img src='pizzaImg.png'/>"+pizzas[d]+"</div>"
+    }
+    strawberry=strawberry+"</section>"
     pizzas.sort();
-    document.getElementById("display_addedmenu").innerHTML = pizzas;
-    console.log(pizzas);
+    document.getElementById("display_addedmenu").innerHTML = strawberry;
+    
 }
